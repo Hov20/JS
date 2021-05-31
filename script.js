1f06e120e98db419fa738bdc18651f3fb7f6d26b
Hovo Harutyunyan <HovHyan16@gmail.com>
2021-05-31 19:36:32 +0800

    rab

---
+++ script.js
@@ -0,0 +1,71 @@
+// This is a comment
+/*
+This is a comment
+This is a comment
+This is a comment
+*/
+// alert('Hello world');
+// alert('Hello');
+// let Username = 'Hovo'; // 'text' "text" -> type string
+// let age = 30; // number 1, -4, 2.34 
+// let isMarried = true; // boolean true, false
+// const lastName = 'Harutyunyan';
+// const COLOR_RED = 'red';
+// var hascar = false;
+
+// alert(Username);
+// alert(age);
+// alert(isMarried);
+// alert(lastName);
+// alert(COLOR_RED);
+// alert(hascar);
+
+let x = 1;
+let y = 5;
+
+// alert(x/0); // Infinity
+// alert('Hello'/2); // NaN -> Not a Number
+// alert(1+1); // 2
+// alert('1'+1); // 11
+let sum = x + y;
+alert(sum);
+
+let testresult = null;
+let notdefinedresult;
+
+// alert(notdefinedresult);
+
+// let name = 'dub';
+// let age = 10;
+// let name2 = 'gug';
+// let age2 = 15;
+
+
+let firstuser = {
+    name: 'big',
+    age: '10',
+    eyecolor: 'blue'
+};
+
+let seconduser = {
+    name: 'dig',
+    age: '20',
+    eyecolor: 'green'
+};
+
+let myarray = ['Hovo', true, {name: 'dub'}];
+
+alert(myarray);
+
+// Refference type vs primitive
+
+
+// Data types
+/**
+ * 1) String
+ * 2) Number
+ * 3) Boolean
+ * 4) null
+ * 5) undefined
+ * 6) object
+ */
